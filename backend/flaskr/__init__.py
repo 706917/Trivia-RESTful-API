2e14a2dc2a0c5ad2e82ab8c3574ce8f3cb062593
Alex <githubalexrom@gmail.com>
2020-03-03 15:10:16 -0500

    Defined first controller and base function
    - create_app()
    - paginate_questions()
    - @app.route('/categories')

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -4,31 +4,64 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from models import setup_db, Question, Category
+from backend.models import setup_db, Question, Category
 
+# Constant with the number fof questions to show in one page of listing
 QUESTIONS_PER_PAGE = 10
 
+
+# Functions to return the list of question to show on specified page of listing
+# Accepts :
+# - 'request' body to get the value of the 'page' parameter
+# - 'selection' as object of result of database query
+# Returns: list of objects to show in specified page
+def paginate_questions(request, selection):
+    # retrieve the page value from the request
+    page = request.args.get('page', 1, type=int)
+    # Define start and end indexes of the list of objects to return
+    start = (1 - page) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    # Format every question-representation we got in the selection object
+    # NOTE : format() function defined in 'Question' class
+    questions = [item.format() for item in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    # Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    CORS(app, resources={r'*': {"origins": "*"}})
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, DELETE, PATCH, OPTIONS, PUT')
+        return response
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+    # An endpoint to handle GET requests  for all available categories.
+    @app.route('/categories', methods=['GET'])
+    def get_categories():
+        # Query DB for all categories
+        selection = Category.query.order_by(id).all()
+        # Format the representation of category to be showed
+        categories = [item.format() for item in selection]
 
+        # Stop and return 404 error if no categories found
+        if len(categories) == 0:
+            abort(404)
 
-  '''
+        return jsonify({
+            'success': True,
+            'categories': categories
+        })
+
+"""
   @TODO: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
@@ -39,9 +72,9 @@ def create_app(test_config=None):
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
-  '''
+  """
 
-  '''
+    '''
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
 
@@ -49,7 +82,7 @@ def create_app(test_config=None):
   This removal will persist in the database and when you refresh the page. 
   '''
 
-  '''
+    '''
   @TODO: 
   Create an endpoint to POST a new question, 
   which will require the question and answer text, 
@@ -60,7 +93,7 @@ def create_app(test_config=None):
   of the questions list in the "List" tab.  
   '''
 
-  '''
+    '''
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
   It should return any questions for whom the search term 
@@ -71,7 +104,7 @@ def create_app(test_config=None):
   Try using the word "title" to start. 
   '''
 
-  '''
+    '''
   @TODO: 
   Create a GET endpoint to get questions based on category. 
 
@@ -80,8 +113,7 @@ def create_app(test_config=None):
   category to be shown. 
   '''
 
-
-  '''
+    '''
   @TODO: 
   Create a POST endpoint to get questions to play the quiz. 
   This endpoint should take category and previous question parameters 
@@ -93,12 +125,9 @@ def create_app(test_config=None):
   and shown whether they were correct or not. 
   '''
 
-  '''
+    '''
   @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
-  
-  return app
-
-    
+    return app
