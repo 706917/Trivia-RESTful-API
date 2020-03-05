ea4d4b7d945b6ffebf0736884299216a642a17c9
Alex <githubalexrom@gmail.com>
2020-03-05 09:24:19 -0500

    errorhandlers added

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -150,20 +150,53 @@ def create_app(test_config=None):
             'question': questions.first().format()
         })
 
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': "Don't be so bad, write you request in polite form, please"
+        }), 400
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return jsonify({
+            'success': False,
+            'error': 403,
+            'message': "Wu-ha-ha-ha, good try, buddy, but NO, you are not allowed over there"
+        }), 403
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': "I have no idea where it is"
+        }), 404
+
+    @app.errorhandler(405)
+    def not_allowed_method(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': "Nope, not this driveway"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': "Well, i understand you and you are right in form, but, i can't get it, sorry, dont hate me"
+        }), 422
+
+   
+
+
+
+
     return app
 
-
-'''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
 
 '''
   @TODO: 
